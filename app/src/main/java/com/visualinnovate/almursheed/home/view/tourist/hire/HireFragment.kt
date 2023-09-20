4601c273751d49622f7bc4b9e4809479e49f12af
mohamednassar <m.nassar@visualinnovate.com>
2023-09-21 02:28:07 +0300

    edit api getAllGuidesByDistCityId and getAllDriversByDistCityId in apiService

--- app/src/main/java/com/visualinnovate/almursheed/home/view/tourist/hire/HireFragment.kt
+++ app/src/main/java/com/visualinnovate/almursheed/home/view/tourist/hire/HireFragment.kt
@@ -8,7 +8,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DefaultItemAnimator
 import com.bumptech.glide.Glide
